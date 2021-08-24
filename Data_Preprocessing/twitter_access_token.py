7952f9ddaf864c61ce3a88166ac4497fa2c4e4f7
I536956 <anurag.watane@sap.com>
2021-08-24 15:14:56 +0200

    remove twitter access keys

--- Data_Preprocessing/twitter_access_token.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "zBhU2ABwEJMC7oiluWB9dXom8"
-consumer_secret = "ZGnVqfbHi9QBJ1mKIdc1MNHlnwL7UCxb275gRpdYkuSMbdvCkM"
-access_token = "1387850521428516864-6NKGM8KxPU1XET2Ffe9RxKXGWsWbFS"
-access_token_secret = "TC1PMWRTvzbS2ORqgV4BC8ADkADrU7s0bqCLeJQw9idpq"
